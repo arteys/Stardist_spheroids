e97e4d5b11161b1563c503f4b6d18893d31679ef
arteys <calamatica@gmail.com>
2022-12-27 23:33:12 +0500

    first commit

---
+++ Stardist.py
@@ -0,0 +1,30 @@
+from stardist.data import test_image_nuclei_2d
+from stardist.plot import render_label
+from csbdeep.utils import normalize
+import matplotlib.pyplot as plt
+import numpy as np
+
+from stardist.models import StarDist2D
+
+# prints a list of available models
+StarDist2D.from_pretrained()
+
+# creates a pretrained model
+# model = StarDist2D.from_pretrained('2D_versatile_fluo')
+
+# img = test_image_nuclei_2d()
+# print(np.shape(img))
+
+# labels, _ = model.predict_instances(normalize(img))
+
+# plt.subplot(1,2,1)
+# plt.imshow(img, cmap="gray"
+# plt.axis("off")
+# plt.title("input image")
+
+# plt.subplot(1,2,2)
+# plt.imshow(render_label(labels, img=img))
+# plt.axis("off")
+# plt.title("prediction + input overlay")
+
+# plt.show()
