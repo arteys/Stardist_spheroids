977930b2fc664c987a4318f427666c02f01895a0
arteys <calamatica@gmail.com>
2023-01-05 19:59:36 +0500

    Good enough  version

--- Stardist.py
+++ Stardist.py
@@ -19,17 +19,16 @@ def file_folder_name(path):
 # prints a list of available models
 # StarDist2D.from_pretrained()
 
-paths = ["C:/Users/Modern/Desktop/Python/Stardist/Images/ApNec_300-20_3.tif"]
-# root = tk.Tk()
-# path = fd.askopenfilenames(parent=root, title='Open images')
-# print(path)
+# paths = ["C:/Users/Modern/Desktop/Python/Stardist/Images/ApNec_300-20_3.tif"]
+root = tk.Tk()
+paths = fd.askopenfilenames(parent=root, title='Open images')
+
 # paths = ["C:/Users/Modern/Desktop/Python/Stardist/Images/ApNec_300-20_3.tif",
-# "C:/Users/Modern/Desktop/Python/Stardist/Images/ApNec_600-20_3.tif",
-# "C:/Users/Modern/Desktop/Python/Stardist/Images/ApNec_600-20_1.tif" ]
+# "C:/Users/Modern/Desktop/Python/Stardist/Images/ApNec 600-20 3.tif",
+# "C:/Users/Modern/Desktop/Python/Stardist/Images/ApNec 600-20 1.tif" ]
 
 model = StarDist2D.from_pretrained('2D_versatile_fluo')
 for p in paths:
-    image_name = os.path.basename(p)
     mylist = []
     loaded,images = cv2.imreadmulti(mats = mylist, filename = p, flags = cv2.IMREAD_ANYCOLOR )
     image_violet, image_green, image_red = images[0],images[3],images[4]
@@ -40,48 +39,13 @@ for p in paths:
 
     name_suffixes = ['violet','green','red']
 
-    filename = 
+    folder,image_name = file_folder_name(p)
+    filename_violet = folder + "/Masks/" + image_name + 'violet' + '.tif'
+    filename_green = folder + "/Masks/" + image_name + 'green' + '.tif'
+    filename_red = folder + "/Masks/" + image_name + 'red' + '.tif'
 
-    cv2.imwrite(filename_violet, labels_green)
+    print(filename_green)
+
+    cv2.imwrite(filename_violet, labels_violet)
     cv2.imwrite(filename_green, labels_green)
-    cv2.imwrite(filename_red, labels_green)
-
-# cv2.imshow('image',labels_violet)
-# cv2.imshow('image',labels_red)
-# cv2.imshow('image',labels_green)
-# cv2.waitKey(0)
-
-plt.subplot(1,2,2)
-# plt.imshow(render_label(labels, img=img))
-plt.imshow(labels_green)
-plt.axis("off")
-plt.title("prediction + input overlay")
-plt.show()
-
-
-# image = images[3]
-# img = image[:,:,2]
-# print(np.shape(img))
-
-# cv2.imshow("mylist[1]",img[:,:,2])
-# cv2.waitKey()
-
-# creates a pretrained model
-
-
-# labels, _ = model.predict_instances(normalize(img)) 
-
-# # print(np.shape(labels))
-
-# plt.subplot(1,2,1)
-# plt.imshow(img, cmap="gray")
-# plt.axis("off")
-# plt.title("input image")
-
-# plt.subplot(1,2,2)
-# # plt.imshow(render_label(labels, img=img))
-# plt.imshow(labels)
-# plt.axis("off")
-# plt.title("prediction + input overlay")
-
-# plt.show()
+    cv2.imwrite(filename_red, labels_red)
