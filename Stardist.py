b5d199454af2f671ca9f6502b5377a90ea921750
arteys <calamatica@gmail.com>
2023-01-05 19:35:19 +0500

    Add filename function

--- Stardist.py
+++ Stardist.py
@@ -7,6 +7,14 @@ import cv2
 import tkinter as tk
 import tkinter.filedialog as fd
 from stardist.models import StarDist2D
+import os
+
+def file_folder_name(path):
+    imagename_without_ext = os.path.splitext(os.path.basename(path))[0]
+    dirname = os.path.dirname(path)
+
+    return dirname, imagename_without_ext
+
 
 # prints a list of available models
 # StarDist2D.from_pretrained()
@@ -21,15 +29,22 @@ paths = ["C:/Users/Modern/Desktop/Python/Stardist/Images/ApNec_300-20_3.tif"]
 
 model = StarDist2D.from_pretrained('2D_versatile_fluo')
 for p in paths:
+    image_name = os.path.basename(p)
     mylist = []
     loaded,images = cv2.imreadmulti(mats = mylist, filename = p, flags = cv2.IMREAD_ANYCOLOR )
-    image_violet = images[0]
-    image_green = images[3]
-    image_red = images[4]
+    image_violet, image_green, image_red = images[0],images[3],images[4]
 
     labels_violet, _ = model.predict_instances(normalize(image_violet[:,:,2]))
     labels_green, _ = model.predict_instances(normalize(image_green[:,:,2]))
     labels_red, _ = model.predict_instances(normalize(image_red[:,:,2]))   
+
+    name_suffixes = ['violet','green','red']
+
+    filename = 
+
+    cv2.imwrite(filename_violet, labels_green)
+    cv2.imwrite(filename_green, labels_green)
+    cv2.imwrite(filename_red, labels_green)
 
 # cv2.imshow('image',labels_violet)
 # cv2.imshow('image',labels_red)
